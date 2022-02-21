f6f9ec57ef37138e9bcc825330a6ab620178940b
Rianer <75773955+Rianer@users.noreply.github.com>
2022-02-21 17:17:48 +0200

    Adaugare comentarii

--- Lab_1/Main.java
+++ Lab_1/Main.java
@@ -1,11 +1,11 @@
 package com.company;
 
 public class Main {
-    public static int generateNum() {
+    public static int generateNum() { //Este generat numarul n
         return (int) (Math.random() * 1_000_000);
     }
 
-    public static int modifyNum(int n) {
+    public static int modifyNum(int n) { // Sunt aplicate calculele de modificare ale lui n
         n = n * 3;
         n = n + 21;
         n = n + 255;
@@ -13,7 +13,8 @@ public class Main {
         return n;
     }
 
-    public static int computeResult(int n) {
+    public static int computeResult(int n) { // Sunt efectuate adunarile cifrelor numarului n
+                                             // pana la obtinerea unei singure cifre
         int result = n;
         while (result / 10 != 0) {
             n = result;
@@ -26,8 +27,8 @@ public class Main {
         return result;
     }
 
-    public static void displayResult(String[] languages) {
-        int n = computeResult(modifyNum(generateNum()));
+    public static void displayResult(String[] languages) { // Se afiseaza rezultatul la ecran
+        int n = computeResult(modifyNum(generateNum())); // Este calculat indexul
         System.out.println("Willy-nilly, this semester I will learn " + languages[n]);
     }
 
