e8cc3522109b1e46eb3b3dd9f3948d9df278fca5
Rianer <75773955+Rianer@users.noreply.github.com>
2022-03-21 17:23:20 +0200

    Update

--- Lab_5/src/main/java/com/company/Main.java
+++ Lab_5/src/main/java/com/company/Main.java
@@ -3,9 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	
         Catalog myCatalog = new Catalog();
-        myCatalog.add(new Book("book2","My Book", "Me", "http://mysite.com/book"));
+        myCatalog.add(new Book("book1","My Book", "Me", "http://mysite.com/book"));
+        myCatalog.add(new Book("book2","My Other Book", "Me", "http://mysite.com/book2"));
         myCatalog.save();
         //myCatalog.load("C:\\Documents\\Java\\Lab5");
     }
