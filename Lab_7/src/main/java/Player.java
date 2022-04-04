22b36451b8d98f6e8bc5bd6cd95bf41b86bf5490
Rianer <75773955+Rianer@users.noreply.github.com>
2022-04-04 16:59:27 +0300

    Update Player.java

--- Lab_7/src/main/java/Player.java
+++ Lab_7/src/main/java/Player.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+    private String name;
+    private int score;
+    private List<Tile> letters;
+
+    public Player(String name, int score, List<Tile> letters) {
+        this.name = name;
+        this.score = score;
+        this.letters = letters;
+    }
+
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+        this.letters = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public List<Tile> getLetters() {
+        return letters;
+    }
+
+    public void setLetters(List<Tile> letters) {
+        this.letters = letters;
+    }
 }
