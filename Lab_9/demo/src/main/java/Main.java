caa50079af60f463b14d04b767e5bbfd0d53100d
Rianer <75773955+Rianer@users.noreply.github.com>
2022-05-08 12:23:22 +0300

    Setup

---
+++ Lab_9/demo/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import dto.Continent;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main (String[] argv){
+
+    }
+}
